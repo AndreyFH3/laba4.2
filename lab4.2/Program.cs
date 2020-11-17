82cd2712826f3d8a01465a1ffc06d729cdd442bd
AndreyFH3 <andrey.likhnitskiy@gmail.com>
2020-11-17 20:31:58 +0700

    makePlus
    maek plus

--- lab4.2/Program.cs
+++ lab4.2/Program.cs
@@ -6,8 +6,15 @@ namespace lab4._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите первое число");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int B = int.Parse(Console.ReadLine());
+            int vari = int.Parse(Console.ReadLine());
+            void pluss(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
         }
     }
 }
